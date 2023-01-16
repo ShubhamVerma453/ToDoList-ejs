8366a4ef0b42c92be2b26ee7b12204bd85dfe70d
ShubhamVerma453 <sv4537373@gmail.com>
2023-01-16 22:21:30 +0530

    basic setup of code done

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: "true" }));
+
+app.get("/", (req, res) => {
+    let date = new Date();
+    let dayNo = date.getDay();
+    let day;
+    switch (dayNo) {
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+            break;
+        default:
+            break;
+    }
+    res.render("index", { ejs: day});
+});
+
+app.listen(3000, () => {
+    console.log("listening");
+});
