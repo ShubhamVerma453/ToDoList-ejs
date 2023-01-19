7ae2efbdb43f9907f47d6004c29a39d522e68a0b
ShubhamVerma453 <sv4537373@gmail.com>
2023-01-19 23:47:59 +0530

    Completed

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 let items = [];
+let work = [];
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -9,19 +10,30 @@ app.use(bodyParser.urlencoded({ extended: "true" }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
+    
     let date = new Date();
-    let option= {
-        weekday : "long",
-        day : "numeric",
-        month : "long"
+    let option = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
     }
-    let day = date.toLocaleDateString("en-US", option);
-    res.render("index", { title: day, list: items});
+    let currTitle = date.toLocaleDateString("en-US", option);
+    res.render("index", { title: currTitle, list: items });
 });
-
-app.post("/", (req, res)=>{
-    items.push( req.body.item);
-    res.redirect("/");
+app.post("/", (req, res) => {
+    if (req.body.btn == "Work") {
+        console.log("work");
+        work.push(req.body.item);
+        res.redirect("/work");
+    } else {
+        console.log("item");
+        items.push(req.body.item);
+        res.redirect("/");
+    }
+});
+app.get("/work", (req, res) => {
+    currTitle = "Work List";
+    res.render("index", { title: currTitle, list: work });
 });
 
 app.listen(3000, () => {
